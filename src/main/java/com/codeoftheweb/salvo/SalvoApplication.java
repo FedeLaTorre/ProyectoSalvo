73655279527ea3b5f6a060af5d6d64000a4b7349
Federico La Torre <fedelatorre.6@gmail.com>
2019-09-23 16:59:11 -0300

    tarea1 finalizada

---
+++ src/main/java/com/codeoftheweb/salvo/SalvoApplication.java
@@ -0,0 +1,30 @@
+package com.codeoftheweb.salvo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class SalvoApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SalvoApplication.class, args);
+	}
+	@Bean
+	public CommandLineRunner iniData (PlayerRepository playerRepository){
+		return	(args) -> {
+			Player p1 = new Player("j.bauer@ctu.gov");
+			Player p2 = new Player("c.obrian@ctu.gov");
+			Player p3 = new Player("kim_bauer@gmail.com");
+			Player p4 = new Player("t.almeida@ctu.gov");
+
+
+			playerRepository.saveAll(Arrays.asList(p1,p2,p3,p4));
+
+		};
+	}
+}
